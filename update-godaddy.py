e2424b1fb68ae4b0de2e81148112b23f631254b4
Connor Berry <cdb979@gmail.com>
2018-01-12 16:58:01 -0500

    Changed TTL to optional argument.

--- update-godaddy.py
+++ update-godaddy.py
@@ -13,14 +13,15 @@ parser = argparse.ArgumentParser(description="A script to dynamically update and
 parser.add_argument("site", help="The fully qualified domain name of the particular site.", type=str)
 parser.add_argument("typ", help="The type of the record that is being updated. A, AAAA, MX, SRV, etc.", type=str)
 parser.add_argument("record", help="Actual record name, i.e. for gmail.google.com the record would be gmail. If the specified record does not exist, one will be created.", type=str)
-parser.add_argument("ttl", help="Time to live, in seconds. Cannot be lower than 600.", type=int)
-parser.add_argument("--auth", help="Your API key.", type=str)
+parser.add_argument("-t", "--ttl", help="Time to live, in seconds. Cannot be lower than 600.", type=int)
+parser.add_argument("-a", "--auth", help="Your API key:secret pair.", type=str)
 
 args = parser.parse_args()
 if not args.auth:
     auth=getpass.getpass()
 else:
     auth=args.auth
+    
 
 url = "https://api.godaddy.com/v1/domains/"+args.site+"/records/"+args.typ+"/"+args.record+"/"
 
@@ -28,13 +29,21 @@ headers = {
     'content-type': 'application/json',
     'Authorization': 'sso-key '+auth
     }
-data = [
-    {"data":client_ip, "ttl":args.ttl}
+if args.ttl and args.ttl >= 600:
+    data = [
+        {"data":client_ip, "ttl":args.ttl}
+    ]
+elif args.ttl and args.ttl <= 600:
+    print("Error: TTL lower than acceptable threshold.")
+    exit(1)
+else:
+    data = [
+        {"data":client_ip}
     ]
 r = requests.put(url, headers=headers, data=json.dumps(data))
 res = r.text
-if res == "{}":
+if res == "{}" and r.status_code == 200:
     exit(0)
-else:
-    print(res)
+print(res)
 r.raise_for_status()
+exit(1)

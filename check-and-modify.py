7f65731031a764d75976fd4f21b518e1a4bec6b8
Connor Berry <cdb979@gmail.com>
2018-01-27 17:37:52 -0500

    Added a new script for checking records before modification

---
+++ check-and-modify.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+import requests, json
+import argparse
+import getpass
+from urllib.request import urlopen
+import sys
+parser = argparse.ArgumentParser(description="A script to check a specified DNS record matches the IP for this host; and if it does not, update that IP with the ip of this host.")
+parser.add_argument("site", help="The fully qualified domain name of the particular site.", type=str)
+parser.add_argument("typ", help="The type of the record that is being updated, caps-sensitive. A, AAAA, MX, SRV, etc.", type=str)
+parser.add_argument("record", help="Actual record name, i.e. for gmail.google.com the record would be gmail.", type=str)
+parser.add_argument("-ip", help="An IP to use in lieu of the one that is fetched by this script. Useful if this machine is not the desired host for your DNS records. (More secure option, can implement your own \"IP getting\" methods instead of the one used in this script.)", type=str)
+parser.add_argument("-t", "--ttl", help="Time to live, in seconds. Cannot be lower than 600.", type=int)
+parser.add_argument("-a", "--auth", help="Your API key:secret pair.", type=str)
+parser.add_argument("-nr", help='"Not really" (will fetch but will not make changes to DNS records)', action="store_true")
+args = parser.parse_args()
+#if not provided with a key & secret from the command line, prompt for one unix-style
+if not args.auth:
+    auth=getpass.getpass()
+#assume the one they provided via CLI
+else:
+    auth=args.auth
+if not args.ip:
+    #get client ip (insecure)
+    client_ip = str(urlopen("https://api.ipify.org").read())
+    client_ip = client_ip.replace("b", "")
+    client_ip = client_ip.replace("'", "")
+else:
+    #use the one provided
+    client_ip = args.ip
+#provided TTL and within acceptable bounds
+if args.ttl and args.ttl >= 600:
+    data = [
+        {"data":client_ip, "ttl":args.ttl}
+    ]
+#not provided or unacceptable uses default
+else:
+    data = [
+        {"data":client_ip}
+    ]
+#form the headers
+headers = {
+    'content-type': 'application/json',
+    'Authorization': 'sso-key '+auth
+    }
+#form the composite URL to use with GoDaddy API
+url = "https://api.godaddy.com/v1/domains/"+args.site+"/records/"
+#send a GET for the record data
+r = requests.get(url, headers=headers)
+if r.status_code != 200:
+    sys.stderr.write("There was a problem accessing your domain records:")
+    sys.stderr.write(r.text)
+    exit(1)
+res = r.json()
+for item in res:
+    if item['name'] == args.record and item['type'] == args.typ:
+        print(item)
+        if item['data'] == client_ip:
+            #no action
+            print("Client IP matches record IP (no action taken)")
+            exit(0)
+        else:
+            #have to change the records
+            print("Client IP ("+client_ip+") does not match record IP ("+item['data']+")")
+            #if we aren't doing a dry-run
+            if not args.nr:
+                #account for @ symbol
+                args.record = args.record.replace("@", "%40")
+                #form new composite url
+                url = "https://api.godaddy.com/v1/domains/"+args.site+"/records/"+args.typ+"/"+args.record+"/"
+                #ship a new request (put this time)
+                r = requests.put(url, headers=headers, data=json.dumps(data))
+                #gain result
+                res = r.text
+                #error checking goes here
+                if res == "{}" and r.status_code == 200:
+                    print("Successfully changed DNS record data to "+client_ip)
+                    exit(0)
+                #print error and status if not
+                sys.stderr.write(res)
+                r.raise_for_status()
+                exit(1)
+            else:
+                print("Not changing record (nr mode)")
+                exit(0)
+sys.stderr.write("Record not found")
+exit(1)
+
+
--- update-godaddy.py
+++ update-godaddy.py
@@ -3,20 +3,20 @@ import requests, json
 import argparse
 import getpass
 from urllib.request import urlopen
+import sys
 #modeled from the following curl statement:
 #curl -XPUT -H "Content-type: application/json" -H 'Authorization: sso-key <API_KEY>' -d "[{\"data\": \"$IP\",\"ttl\": $TTL}]" "https://api.godaddy.com/v1/domains/$SITE/records/"$TYPE"/$RECORD"
 
-#get our client ip dynamically by contacting an outside server (technically insecure, more on this later)
-client_ip = str(urlopen("https://api.ipify.org").read())
-client_ip = client_ip.replace("b", "")
-client_ip = client_ip.replace("'", "")
+
 
 parser = argparse.ArgumentParser(description="A script to dynamically update and/or create your site's GoDaddy DNS records.")
 parser.add_argument("site", help="The fully qualified domain name of the particular site.", type=str)
-parser.add_argument("typ", help="The type of the record that is being updated. A, AAAA, MX, SRV, etc.", type=str)
+parser.add_argument("typ", help="The type of the record that is being updated, caps-sensitive. A, AAAA, MX, SRV, etc.", type=str)
 parser.add_argument("record", help="Actual record name, i.e. for gmail.google.com the record would be gmail. If the specified record does not exist, one will be created.", type=str)
 parser.add_argument("-t", "--ttl", help="Time to live, in seconds. Cannot be lower than 600.", type=int)
+parser.add_argument("-ip", help="An IP to use in lieu of the one that is fetched by this script. Useful if this machine is not the desired host for your DNS records. (More secure option, can implement your own \"IP getting\" methods instead of the one used in this script.)", type=str)
 parser.add_argument("-a", "--auth", help="Your API key:secret pair.", type=str)
+parser.add_argument("-nr", help='"Not really" (will fetch but will not make changes to DNS records)', action="store_true")
 
 args = parser.parse_args()
 #if not provided with a key & secret from the command line, prompt for one unix-style
@@ -25,12 +25,19 @@ if not args.auth:
 #assume the one they provided via CLI
 else:
     auth=args.auth
-    
+if not args.ip:
+    #get client ip (insecure)
+    client_ip = str(urlopen("https://api.ipify.org").read())
+    client_ip = client_ip.replace("b", "")
+    client_ip = client_ip.replace("'", "")
+else:
+    #use the one provided
+    client_ip = args.ip    
 #need to account for the @ symbol being a pain in HTML
 args.record = args.record.replace("@", "%40") 
-#form the compostite URL to be used with GoDaddy's api (v1)
+#form the compostite URL to be used with GoDaddy's API (v1)
 url = "https://api.godaddy.com/v1/domains/"+args.site+"/records/"+args.typ+"/"+args.record+"/"
-
+#form the headers
 headers = {
     'content-type': 'application/json',
     'Authorization': 'sso-key '+auth
@@ -40,8 +47,8 @@ if args.ttl and args.ttl >= 600:
         {"data":client_ip, "ttl":args.ttl}
     ]
 #save making a connection just for it to bounce back at us
-elif args.ttl and args.ttl <= 600: 
-    print("Error: TTL lower than acceptable threshold.") 
+elif args.ttl and args.ttl < 600: 
+    sys.stderr.write("Error: TTL lower than acceptable threshold.") 
     #should really have that print to stderr though
     exit(1)
 #assuming they did not provide a TTL (GoDaddy can still handle without one)
@@ -49,13 +56,16 @@ else:
     data = [
         {"data":client_ip}
     ]
-#ship the request
-r = requests.put(url, headers=headers, data=json.dumps(data))
-res = r.text #gather the result
-#perform some insurance that it executed sucessfully
-if res == "{}" and r.status_code == 200: 
-    exit(0)
-#print the error and status if not
-print(res) 
-r.raise_for_status()
-exit(1)
+if not args.nr:
+    #ship the request
+    r = requests.put(url, headers=headers, data=json.dumps(data))
+    res = r.text #gather the result
+    #perform some insurance that it executed sucessfully
+    if res == "{}" and r.status_code == 200: 
+        exit(0)
+    #print the error and status if not
+    sys.stderr.write(res) 
+    r.raise_for_status()
+    exit(1)
+else:
+    print("(nr mode)", url, headers, data)

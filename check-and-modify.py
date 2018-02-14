2c5264718615930f8855c7e957ed7f39b828f0ef
Connor Berry <34040804+cdb84@users.noreply.github.com>
2018-02-14 17:08:12 -0500

    Refactored variables to more verbose options

--- check-and-modify.py
+++ check-and-modify.py
@@ -4,32 +4,35 @@ import argparse
 import getpass
 from urllib.request import urlopen
 import sys
-ip_fetch = "https://api.ipify.org"
+_ip_fetch_ = "https://api.ipify.org"
+_api_url_front_ = "https://api.godaddy.com/v1/domains/"
+_ttl_low_bound_ = 600
 parser = argparse.ArgumentParser(description="A script to check a specified DNS record matches the IP for this host; and if it does not, update that IP with the ip of this host.")
 parser.add_argument("site", help="The fully qualified domain name of the particular site.", type=str)
+#this argument needs to be tidied up--it should be "type", but do we know if we can call it that in python without language conflicts?
 parser.add_argument("typ", help="The type of the record that is being updated, caps-sensitive. A, AAAA, MX, SRV, etc.", type=str)
 parser.add_argument("record", help="Actual record name, i.e. for gmail.google.com the record would be gmail.", type=str)
 parser.add_argument("-ip", help="An IP to use in lieu of the one that is fetched by this script. Useful if this machine is not the desired host for your DNS records. (More secure option, can implement your own \"IP getting\" methods instead of the one used in this script.)", type=str)
-parser.add_argument("-t", "--ttl", help="Time to live, in seconds. Cannot be lower than 600.", type=int)
+parser.add_argument("-t", "--ttl", help="Time to live, in seconds. Cannot be lower than 600. GoDaddy default is 1 hour (3600 seconds)", type=int)
 parser.add_argument("-a", "--auth", help="Your API key:secret pair.", type=str)
 parser.add_argument("-nr", help='"Not really" (will fetch but will not make changes to DNS records)', action="store_true")
 args = parser.parse_args()
 #if not provided with a key & secret from the command line, prompt for one unix-style
 if not args.auth:
-    auth=getpass.getpass()
+    auth = getpass.getpass()
 #assume the one they provided via CLI
 else:
-    auth=args.auth
+    auth = args.auth
 if not args.ip:
     #get client ip (insecure)
-    client_ip = str(urlopen(ip_fetch).read())
+    client_ip = str(urlopen(_ip_fetch_).read())
     client_ip = client_ip.replace("b", "")
     client_ip = client_ip.replace("'", "")
 else:
     #use the one provided
     client_ip = args.ip
 #provided TTL and within acceptable bounds
-if args.ttl and args.ttl >= 600:
+if args.ttl and args.ttl >= _ttl_low_bound_:
     data = [
         {"data":client_ip, "ttl":args.ttl}
     ]
@@ -46,13 +49,13 @@ headers = {
 #form the composite URL to use with GoDaddy API
 url = "https://api.godaddy.com/v1/domains/"+args.site+"/records/"
 #send a GET for the record data
-r = requests.get(url, headers=headers)
-if r.status_code != 200:
+req = requests.get(url, headers=headers)
+if req.status_code != 200:
     sys.stderr.write("There was a problem accessing your domain records:")
-    sys.stderr.write(r.text)
+    sys.stderr.write(req.text)
     exit(1)
-res = r.json()
-for item in res:
+records = req.json()
+for item in records:
     if item['name'] == args.record and item['type'] == args.typ:
         print(item)
         if item['data'] == client_ip:
@@ -69,19 +72,19 @@ for item in res:
                 #form new composite url
                 url = "https://api.godaddy.com/v1/domains/"+args.site+"/records/"+args.typ+"/"+args.record+"/"
                 #ship a new request (put this time)
-                r = requests.put(url, headers=headers, data=json.dumps(data))
+                req = requests.put(url, headers=headers, data=json.dumps(data))
                 #gain result
-                res = r.text
+                result = req.text
                 #error checking goes here
-                if res == "{}" and r.status_code == 200:
+                if result == "{}" and req.status_code == 200:
                     print("Successfully changed DNS record data to "+client_ip)
                     exit(0)
                 #print error and status if not
                 sys.stderr.write(res)
-                r.raise_for_status()
+                req.raise_for_status()
                 exit(1)
             else:
-                print("Not changing record (nr mode)")
+                print("Not changing record (-nr mode)")
                 exit(0)
 sys.stderr.write("Record not found")
 exit(1)
